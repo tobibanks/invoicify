e736c6061fe4c3719d9e5352760d99dfcb362466
tobibanks <tobiodogwu@gmail.com>
2025-02-03 07:41:19 +0100

    first push

---
+++ src/app/invoices/new/page.tsx
@@ -0,0 +1,82 @@
+"use client";
+
+import Form from "next/form";
+import { type SyntheticEvent, useState } from "react";
+
+import Container from "@/components/Container";
+import SubmitButton from "@/components/SubmitButton";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Textarea } from "@/components/ui/textarea";
+
+import { createAction } from "@/app/actions";
+
+export default function Home() {
+  const [state, setState] = useState("ready");
+  const [error, setError] = useState<string>();
+
+  async function handleOnSubmit(event: SyntheticEvent) {
+    // Submission disabled for demo
+  //  event.preventDefault();
+   // setError('Submission disabled for demo.');
+   // return;
+
+    if (state === "pending") {
+      event.preventDefault();
+      return;
+    }
+   // setState("pending");
+  }
+
+  return (
+    <main className="h-full">
+      <Container>
+        {error && (
+          <p className="bg-red-100 text-sm text-red-800 text-center px-3 py-2 rounded-lg mb-6">
+            { error }
+          </p>
+        )}
+        <div className="flex justify-between mb-6">
+          <h1 className="text-3xl font-semibold">Create Invoice</h1>
+        </div>
+
+        <Form
+          action={createAction}
+          onSubmit={handleOnSubmit}
+          className="grid gap-4 max-w-xs"
+        >
+          <div>
+            <Label htmlFor="name" className="block font-semibold text-sm mb-2">
+              Billing Name
+            </Label>
+            <Input id="name" name="name" type="text" />
+          </div>
+          <div>
+            <Label htmlFor="email" className="block font-semibold text-sm mb-2">
+              Billing Email
+            </Label>
+            <Input id="email" name="email" type="email" />
+          </div>
+          <div>
+            <Label htmlFor="value" className="block font-semibold text-sm mb-2">
+              Value
+            </Label>
+            <Input id="value" name="value" type="text" />
+          </div>
+          <div>
+            <Label
+              htmlFor="description"
+              className="block font-semibold text-sm mb-2"
+            >
+              Description
+            </Label>
+            <Textarea id="description" name="description" />
+          </div>
+          <div>
+            <SubmitButton />
+          </div>
+        </Form>
+      </Container>
+    </main>
+  );
+}
