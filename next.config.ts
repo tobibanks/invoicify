821b79d6ea4a5afc91e5a86b54c66affb676d3cc
tobibanks <tobiodogwu@gmail.com>
2025-03-29 16:19:49 +0100

    quick fixes

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,10 @@ const nextConfig: NextConfig = {
     bodySizeLimit: '2mb',
     timeout: 10000, // 10 seconds
   },
-  
+  eslint: {
+    ignoreDuringBuilds: false,
+    dirs: ['src'],
+  }
 };
 
 export default nextConfig;
