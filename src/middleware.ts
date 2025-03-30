6cb1adaa80edeb6523d95a5f9c3ab5613e87347e
tobibanks <tobiodogwu@gmail.com>
2025-03-30 20:25:53 +0100

    quick push

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,31 @@
+// import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+
+// const isPublic = createRouteMatcher([
+//   "/",
+//   "/sign-in(.*)",
+//   "/sign-up(.*)",
+//   "/invoices/(.*)/payment",
+// ]);
+
+// export default clerkMiddleware(async (auth, request) => {
+//   if (!isPublic(request)) {
+//     await auth().protect();
+//   }
+// });
+
+// export const config = {
+//   matcher: [
+//     // Skip Next.js internals and all static files, unless found in search params
+//     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+//     // Always run for API routes
+//     "/(api|trpc)(.*)",
+//   ],
+  
+// };
+
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
+const isPublicRoute = createRouteMatcher(['/sign-in', '/sign-up']);
 const isPublic = createRouteMatcher([
   "/",
   "/sign-in(.*)",
@@ -7,24 +33,12 @@ const isPublic = createRouteMatcher([
   "/invoices/(.*)/payment",
 ]);
 
-// const isProtected = createRouteMatcher([
-//   '/dashboard',
-//   '/invoices/:invoiceId',
-//   '/invoices/new'
-// ])
-
-export default clerkMiddleware(async (auth, request) => {
-  if (!isPublic(request)) {
-    await auth().protect();
+export default clerkMiddleware((auth, request) => {
+  if(!isPublic(request)) {
+    auth().protect();
   }
 });
 
 export const config = {
-  matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    // Always run for API routes
-    "/(api|trpc)(.*)",
-  ],
-  
+  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
 };
